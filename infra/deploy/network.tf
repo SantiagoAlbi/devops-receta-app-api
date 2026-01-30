93aa9bbe7481815e723b830fd7dbe301b29699a6
Santi Albi26 <santi.albisetti>
2026-01-30 08:34:20 -0300

    added vpc

---
+++ infra/deploy/network.tf
@@ -0,0 +1,22 @@
+#########################
+# Network Configuration #
+#########################
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.1.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+}
+
+##########################
+# Internet Gateway Setup #
+##########################
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-main-igw"
+  }
+}
